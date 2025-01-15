b80e6d05b2d2b275feaa9e512b6995e05ee60cf7
Sivakumar N <shivask94423@gmail.com>
2025-01-15 14:33:07 +0530

    Get the information of thanos boards, based on board get lists, and list cards only Mind .

---
+++ test/testCallback4.cjs
@@ -0,0 +1,15 @@
+/* 
+    Problem 4: Write a function that will use the previously written functions to get the following information. 
+    You do not need to pass control back to the code that called it.
+
+    Get information from the Thanos boards
+    Get all the lists for the Thanos board
+    Get all cards for the Mind list simultaneously
+*/
+const getAllInfo = require('../callback4.cjs');
+
+getAllInfo((error) => {
+    if(error) {
+        console.log(error);
+    }
+});
